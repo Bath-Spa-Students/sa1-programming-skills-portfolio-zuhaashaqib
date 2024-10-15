64f0f36f3ad0d4ff3b65f8a3199fbfc61039a9db
zuhaashaqib <zuhaashaqib@gmail.com>
2024-10-15 13:07:09 +0400

    Create Coding Is Cool.py
    Coding Is Cool

---
+++ Exercise 01/Coding Is Cool.py
@@ -0,0 +1,5 @@
+#Using string concatenantion to print the phrase "coding is cool"
+word1 = "Coding "   #assigning values to variables (variable 1)
+word2 = "Is "       #(variable 2)
+word3 = "Cool"      #(variable 3)
+print(word1 + word2 + word3)   #using string concatenation operator '+' to print output
