5269253cdeaaccbca37f22e718f88fa2efb5c08c
zuhaashaqib <zuhaashaqib@gmail.com>
2024-10-17 11:14:09 +0400

    Create Simple Sums.py
    Simple Sums

---
+++ Exercise 02/Simple Sums.py
@@ -0,0 +1,5 @@
+#performing arithmetic operations by using variables
+a = 8   #first variable is assigned with the value '8'
+b = 10  #second variable is assigned with the value '10'
+c = a + b  #third variable is assigned with the sum of first 2 variables
+print(c)   #using print statement for the output
