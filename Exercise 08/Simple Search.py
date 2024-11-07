ac72e8523835030cb2f1d4977bc775c94a38db3c
zuhaashaqib <zuhaashaqib@gmail.com>
2024-11-07 12:26:53 +0400

    Simple Search.py

--- Exercise 08/Simple Search.py
+++ Exercise 08/Simple Search.py
@@ -1,4 +1,4 @@
-#Exercise 8: Simple Search - 30 Marks
+#Exercise 8: Simple Search - 30 Marks 
 '''Write a program that searches for a specific string within a list of strings. The list
 is initialized with specific names (“Jake” “Zac”, “Ian”, “Ron”, “Sam”, “Dave”).
 , and your task is to search for “Sam”.
