4af02030cdcdcebdaf87288875bb7aa3b5ec8dd4
zuhaashaqib <zuhaashaqib@gmail.com>
2024-11-07 12:09:57 +0400

    Some Counting

---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,25 @@
+#Exercise 7: Some Counting - 20 Marks
+'''Use your newly acquired knowledge of the for loop to complete the following
+tasks. Print all values to the console in each case. * Write a loop that counts
+up from 0 to 50 in increments of 1. * Write a loop that counts down from 50 to
+0 in decrements of 1. * Write a loop that counts up from 30 to 50 in increments
+of 1. * Write a loop that counts down from 50 to 10 in decrements of 2. * Write
+a loop that counts up from 100 to 200 in increments of 5. You may include all
+loops in a single project'''
+
+for a in range(0,51,1):       #using range function for counting with increment
+    print(a)                  #print function for output
+ 
+for b in range(50,-1,-1):     #using range function for counting with decrement
+     print(b)                 #print function to get output
+
+for c in range(30,51,1):      #range function 
+    print(c)
+
+for d in range(50,9,-2):      #range function with decrement
+    print(d)
+
+for e in range(100,201,5):    #range function
+    print(e)
+
+
---
+++ Exercise 08/Simple Search.py
@@ -0,0 +1,13 @@
+#Exercise 8: Simple Search - 30 Marks
+'''Write a program that searches for a specific string within a list of strings. The list
+is initialized with specific names (“Jake” “Zac”, “Ian”, “Ron”, “Sam”, “Dave”).
+, and your task is to search for “Sam”.
+Optional Requirements:
+1. Allow the user to input the search term instead of using a predefined value.
+2. Implement the search functionality based on user input.'''
+
+
+boys = ["Jake","Zac","Ian","Ron","Sam","Dave"]  #initialization of list
+if "Sam" in boys:   #condition to check if 'Sam' is in the list
+    print("Yes, Sam is in this list")   #output based on the above condition
+
