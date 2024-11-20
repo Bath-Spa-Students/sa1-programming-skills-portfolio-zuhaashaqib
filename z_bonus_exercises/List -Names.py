81663d6b2b2a9afaf6d97eb0a78785be56e4baa3
zuhaashaqib <zuhaashaqib@gmail.com>
2024-11-20 21:05:57 +0400

    Lists-Names

---
+++ z_bonus_exercises/List -Names.py
@@ -0,0 +1,10 @@
+#List -Names
+'''Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time.'''
+
+friends = ['Simal','Halima','Sukaina','Irsa']       #initializing a list with 4 elements
+print(friends[0])                                   #print the output by using index of each element
+print(friends[1])
+print(friends[2])
+print(friends[3])
+
