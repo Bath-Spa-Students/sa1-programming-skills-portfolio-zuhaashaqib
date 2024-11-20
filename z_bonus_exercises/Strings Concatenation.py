764e43076b9ba8f144a86f9ef159a8e397439e13
zuhaashaqib <zuhaashaqib@gmail.com>
2024-11-20 20:49:26 +0400

    String Concatenation

---
+++ z_bonus_exercises/Strings Concatenation.py
@@ -0,0 +1,7 @@
+#Strings Concatenation:
+'''Write three strings in different variables and print the output as one string.'''
+
+x = "Your "          #1st variable
+y = "age is "        #2nd variable
+z = "18"             #3rd variable
+print(x+y+z)         #using print to output them as one string
---
+++ z_bonus_exercises/Variables.py
@@ -0,0 +1,8 @@
+#Variables:
+'''Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message.'''
+
+var = "Welcome!"         #variable assigned with 1st message
+print(var)               #print for output of message
+var = "Goodbye!"         #variable assigned with 2nd message
+print(var)
