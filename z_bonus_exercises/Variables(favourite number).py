2e8b365e3c67623a8f0ffe3c5d946f639d7de16b
zuhaashaqib <zuhaashaqib@gmail.com>
2024-11-20 21:05:39 +0400

    Variables( favourite number)

---
+++ z_bonus_exercises/Variables(favourite number).py
@@ -0,0 +1,7 @@
+#Variables (Favorite Number):
+'''Use a variable to represent your favorite number. Then, using that variable, create a
+message that reveals your favorite number. Print that message.'''
+
+fav = 27                                           #assigning fav number to a variable
+message = f"My favourite number is {fav}."         #using f-string to reveal number
+print(message)                                     #print function for output
