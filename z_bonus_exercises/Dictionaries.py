c8993419cfa40c0cea4b1167207492742bd05493
zuhaashaqib <zuhaashaqib@gmail.com>
2024-11-20 21:38:54 +0400

    Dictionaries

---
+++ z_bonus_exercises/Dictionaries.py
@@ -0,0 +1,18 @@
+#Dictionaries:
+'''Use a dictionary to store information about a person you know.Store their first name,
+last name, age, and the city in which they live.
+
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary.'''
+
+person = {                         #create dictionary named person
+    'first_name' : 'Zainab',       
+    'last_name' : 'Shaqib',
+    'age' : 17,
+    'city' : 'Sharjah'
+    }
+
+print(person['first_name'])       #print function for output each value of dictionary
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
